e462bf8c6e6946c9c35599c2cd132d7d319f72b0
jacob4649 <jacob.klimczak@gmail.com>
2021-12-13 10:15:14 -0500

    clean tweets

--- tweets.py
+++ tweets.py
@@ -40,33 +40,22 @@ class Tweet(object):
     raw_location: str
     polarity: float
 
-    def __init__(self, row: List[str], analyzer: SentimentIntensityAnalyzer, filtered=False):
+    def __init__(self, row: List[str], analyzer: SentimentIntensityAnalyzer):
         """Create a tweet from a
-        row in a csv of tweets.
-
-        The filtered flag indicates whether to skip checking
-        the validity of inputs, and just assume they are
-        correctly formatted
+        row in a csv of tweets, and a sentiment analyzer to use
+        to analyze the tweet content
 
         Preconditions:
             - len(row) == 13
         """
         self.user = row[0]
         self.tweet = row[9]
-
-        if filtered:
-            self.location = states.code_lookup(row[1])
-            self.followers = int(float(row[4]))
-            self.time_stamp = datetime.strptime(row[8], '%Y-%m-%d %H:%M')
-            self.raw_location = row[2]
-            self.polarity = float(row[3])
-        else:
-            self.location = states.location_lookup(row[1])
-            self.followers = int(float(row[4]))
-            self.time_stamp = _from_csv_date(row[8])
-            self.raw_location = row[1]
-            scores = analyzer.polarity_scores(self.tweet)
-            self.polarity = scores['compound']
+        self.location = states.location_lookup(row[1])
+        self.followers = int(float(row[4]))
+        self.time_stamp = _from_csv_date(row[8])
+        self.raw_location = row[1]
+        scores = analyzer.polarity_scores(self.tweet)
+        self.polarity = scores['compound']
 
 
 def from_csv(path: str, analyzer: SentimentIntensityAnalyzer) -> Iterable[Tweet]:
@@ -80,48 +69,20 @@ def from_csv(path: str, analyzer: SentimentIntensityAnalyzer) -> Iterable[Tweet]
         reader = csv.reader(csv_file)
         next(reader, None)  # Skip header
 
-        with ThreadPoolExecutor(max_workers=10) as reader_executor:
-            # filter out bad rows
-            filtered_rows = filter(_filter_row, reader)
+        # filter out unwanted rows
+        filtered_rows = filter(_filter_row, reader)
+
+        def create_tweet(row: List[str]) -> Tweet:
+            """Return a tweet created from a csv row
+            
+            Function for use with map that creates a
+            tweet from a csv row"""
+            return Tweet(row, analyzer)
+
+        with ProcessPoolExecutor(max_workers=10) as reader_executor:
             # disk io, object initialization, and vader polarity assignment
-            def create_tweet(row): return Tweet(row, analyzer, False)
             for result in reader_executor.map(create_tweet, filtered_rows):
                 yield result
-
-
-def from_filtered_csv(path: str) -> Iterable[Tweet]:
-    """Return a generator for tweets from a csv file.
-
-    Assumes all tweets in the csv file match the selection
-    criteria for this project and that csv contains no header"""
-    with open(path, encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            if _filter_row(row):
-                yield Tweet(row, filtered=True)
-
-
-def filter_and_save(path: str, dest: str, analyzer: SentimentIntensityAnalyzer) -> None:
-    """Save a csv of filtered tweets at the specified
-    destination using tweets from the specified csv.
-
-    Uses the provided sentiment intensity analyzer to determine vader scores
-
-    Does not write a header"""
-    filtered = from_csv(path, analyzer)
-    with open(dest, 'w', encoding='utf-8') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
-        with ThreadPoolExecutor(max_workers=1) as csv_write_queue:
-            for tweet in filtered:
-                csv_write_queue.submit(writer.writerow, _get_row(tweet))
-
-
-def _get_row(tweet: Tweet) -> List[str]:
-    """Return a list of values to write to a csv file
-    representing a tweet"""
-    return [tweet.user, tweet.location.code, tweet.raw_location, tweet.polarity, tweet.followers,
-            '', '', '', tweet.time_stamp.strftime('%Y-%m-%d %H:%M'),
-            tweet.tweet, '', '', '']
 
 
 def _filter_row(row: List[str]) -> bool:
@@ -161,18 +122,6 @@ def _from_csv_date(date: str) -> Optional[datetime]:
         return datetime.strptime(date, '%d-%m-%Y %H:%M:%S')
     except ValueError:
         pass
-
-
-def _print_tweet(tweet: Tweet) -> None:
-    """Print a tweet to the console in a human
-    readable format
-    """
-    date = tweet.time_stamp.strftime('%Y-%m-%d %H:%M')
-    print(f'User: {tweet.user}\n' +
-          f'Location: {tweet.location.name}\n' +
-          f'Followers: {tweet.followers}\n' +
-          f'Date: {date}\n' +
-          f'Tweet: {tweet.tweet}\n')
 
 
 if __name__ == '__main__':
