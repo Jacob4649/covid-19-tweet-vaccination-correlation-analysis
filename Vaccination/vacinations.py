99d63296ea9e74d8a6c605a44778ab88b81fb145
Jacob Klimczak <jacob.klimczak@gmail.com>
2021-12-12 22:46:13 -0500

    rename vaccination rate fields

--- Vaccination/vacinations.py
+++ Vaccination/vacinations.py
@@ -13,14 +13,14 @@ class VaccinationRate:
     Attributes:
         - location: the name of the location
         - time_stamp: the date the data was taken
-        - total_vaccination: the total amount of people vaccinated at that time and location
-        - daily_vaccination: the amount of people vaccinated daily at that location and time
+        - total: the total amount of people vaccinated at that time and location
+        - daily: the amount of people vaccinated daily at that location and time
 
     """
     location: str
     time_stamp: time_stamp
-    total_vaccination: float
-    daily_vaccination: float
+    total: float
+    daily: float
 
     def __init__(self, row: List[str]):
         """Initialize an instance of this vaccination rate class
@@ -30,5 +30,5 @@ class VaccinationRate:
             - len(row) == 13"""
         self.location = row[1]
         self.time_stamp = date.strptime(row[0], '%Y-%m-%d')
-        self.total_vaccination = float(row[2])
-        self.daily_vaccination = float(row[11])
+        self.total = float(row[2])
+        self.daily = float(row[11])
