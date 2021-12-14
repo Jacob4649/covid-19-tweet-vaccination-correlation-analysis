30df0427db2394485f7d26553d781880725dfa5b
Jacob Klimczak <jacob.klimczak@gmail.com>
2021-12-14 04:50:42 -0500

    remove location and code lookup from states

--- states.py
+++ states.py
@@ -12,7 +12,6 @@ This file is Copyright (c) 2021 Jacob Klimczak, Ryan Merheby and Sean Ryan.
 """
 
 import json
-from typing import Optional
 from locations import Location
 
 
@@ -40,52 +39,6 @@ def unpack_json_into_locations(filename: str) -> list[Location]:
         print("The requested file cannot be loaded from.")
 
     return states_so_far
-
-
-def code_lookup(code: str, states_list: list[Location]) -> Location:
-    """Return a location with a code matching the provided one.
-    Preconditions:
-        - len(code) == 2
-        - code.upper() == code
-        - code.isalpha()"""
-    # Binary search for state with matching code
-    # Posible because states are in alphabetical order
-
-    lower = 0
-    upper = len(states_list)
-    pointer = (lower + upper) // 2
-    while 0 <= lower < len(states_list) and \
-            0 <= upper <= len(states_list) and \
-            states_list[pointer].code != code:
-        if code > states_list[pointer].code:
-            lower = pointer
-        else:  # Must be less because loop doesn't run when is equal
-            upper = pointer
-        pointer = (lower + upper) // 2
-    return states_list[pointer]
-
-
-def location_lookup(location: str, states_list: list[Location]) -> Optional[Location]:
-    """Return a location that has either a name,
-    state code, or related term matching the
-    provided location string.
-    Return None if no matching location can be found"""
-    # Exhaust state names, then related terms, before
-    # finally checking for state codes
-    # matches lowercase for state name and
-    # related terms but not state code
-    location_lower = location.lower()
-    for state in states_list:
-        if state.name.lower() in location_lower:
-            return state
-    for state in states_list:
-        for term in state.related_terms:
-            if term.lower() in location_lower:
-                return state
-    for state in states_list:
-        if state.code in location:
-            return state
-    return None
 
 
 if __name__ == '__main__':
