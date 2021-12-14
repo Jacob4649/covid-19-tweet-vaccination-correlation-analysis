3dba3e060d7894d8b9ed18878e7318ad12845648
Jacob Klimczak <jacob.klimczak@gmail.com>
2021-12-14 03:01:15 -0500

    add check contracts

--- data_processing.py
+++ data_processing.py
@@ -366,7 +366,10 @@ def calculate_correlation(tweets: List[float], vaccines: List[float]) -> float:
 
 if __name__ == '__main__':
     import python_ta
+    import python_ta.contracts
 
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
     python_ta.check_all(config={
         'extra-imports': ['numpy',
                           'scipy.stats',
