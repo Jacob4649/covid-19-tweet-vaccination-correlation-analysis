cf84ed52b9a84c615f35af780d72ad1e61b54c2e
jacob4649 <jacob.klimczak@gmail.com>
2021-12-13 20:36:51 -0500

    calculate correlations in main

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from datetime import date
-import math
 from typing import Dict, List, Tuple
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model._glm.glm import _y_pred_deviance_derivative
 from app import App
 import vaccinations
 import tweets
 import visualization
-from data_processing import DailyMetricCollection, average_metrics, generate_metrics, location_dict
+from data_processing import DailyMetricCollection, average_metrics, generate_metrics, location_dict, calculate_correlation
 from tweets import Tweet
 from vaccinations import VaccinationRate
 
@@ -45,15 +41,36 @@ def location_stats(vaccine_dict: Dict[str, List[VaccinationRate]],
     return tweet_list, vaccine_list
 
 
+def location_correlation(vaccine_dict: Dict[str, List[VaccinationRate]],
+                         tweet_dict: Dict[str, List[Tweet]],
+                         start: date, end: date) -> Dict[str, float]:
+    """Return a dictionary mapping state codes to the correlation 
+    between state vaccination and state twitter discourse"""
+    output = {}
+    for key in location_tweets:
+        if key in location_vaccines:
+            a, b = location_stats(
+                location_vaccines, location_tweets, key, start, end)
+            c = calculate_correlation(a, b)
+            output[key] = c
+    return output
+
+
 if __name__ == '__main__':
     app = App()
-    raw_tweets = tweets.from_csv(app.tweet_path, app)
-    raw_vaccinations = vaccinations.from_csv(app.vaccine_path, app)
+    raw_tweets = list(tweets.from_csv(app.tweet_path, app))
+    raw_vaccinations = list(vaccinations.from_csv(app.vaccine_path, app))
 
-    # location_tweets = location_dict(raw_tweets,
-    #                                 lambda tweet: tweet.location)
-    # location_vaccines = location_dict(raw_vaccinations,
-    #                                   lambda rate: rate.location)
+    start = date(2021, 2, 28)
+    end = date(2021, 11, 1)
+
+    location_tweets = location_dict(raw_tweets,
+                                    lambda tweet: tweet.location)
+    location_vaccines = location_dict(raw_vaccinations,
+                                      lambda rate: rate.location)
+
+    correlations = location_correlation(
+        location_vaccines, location_tweets, start, end)
 
     tweet_metric = generate_metrics(
         raw_tweets, lambda tweet: (tweet.time_stamp.date(), tweet.polarity))
@@ -66,16 +83,11 @@ if __name__ == '__main__':
     tweet_collection = DailyMetricCollection(average_tweet_polarity, False)
     vaccine_collection = DailyMetricCollection(average_vaccine_rate, True)
 
-    start = date(2021, 2, 28)
-    end = date(2021, 11, 1)
-
     tweet_range = tweet_collection.get(start, end)
     vaccine_range = vaccine_collection.get(start, end)
 
     tweet_list = list(tweet_range)
     vaccine_list = list(vaccine_range)
-
-    diff = (end - start).days + 1
 
     fig = visualization.vaccination_twitter_plot(
         tweet_list, vaccine_list, 'Vaccination Rate As Related To Ongoing Twitter Discourse')
