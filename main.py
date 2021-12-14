912bfe1f30ad1dda2c2ad63fb7e713331a3197c0
jacob4649 <jacob.klimczak@gmail.com>
2021-12-14 01:45:31 -0500

    finish outputs from main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import App
 import vaccinations
 import tweets
 import visualization
+import plotly.express as px
 from data_processing import DailyMetricCollection, average_metrics, generate_metrics, location_dict, calculate_correlation
 from tweets import Tweet
 from vaccinations import VaccinationRate
@@ -124,7 +125,7 @@ if __name__ == '__main__':
     figures.append(visualization.unwrap_figure(fig.to_html()))
 
     figures.append(visualization.text_block(
-        'The graph above shows the average vaccination rate in the US on a given day ' +
+        'The graph above shows the average vaccination rate per state in the US on a given day ' +
         'as a function of the average sentiment of Twitter discourse on the same day. ' +
         'From this graph we can see that days with higher intensity sentiments (' +
         'reflecting more positive views towards the vaccine) tend to correspond to ' +
@@ -179,6 +180,7 @@ if __name__ == '__main__':
     # modeled second half
     figures.append(visualization.text_block(
         '<div style="text-align: center; font-size: 24pt; padding: 24pt">MODEL PREDICTIONS</div>'))
+
     figures.append(visualization.text_block(
         'Here we will examine how well a linear model could have predicted vaccination ' +
         'rates based on Twitter data at an arbitrary moment in time. The graph below has ' +
@@ -198,5 +200,51 @@ if __name__ == '__main__':
         regression_twitter=half_tweet_list, regression_vaccine=half_vaccine_list)
 
     figures.append(visualization.unwrap_figure(model.to_html()))
+
+    figures.append(visualization.text_block('These results are somewhat surprising, ' +
+                                            'the data used to generate the model can ' +
+                                            'be seen below.'))
+
+    half_x = list(range(len(half_vaccine_list)))
+
+    # half vaccine figure
+    half_vaccine_fig = px.scatter(x=half_x, y=half_vaccine_list,
+                                  title='Vaccination Rate Across US (Until Halfway Point)',
+                                  labels=dict(x='Days Since 2021-02-28',
+                                              y='Mean Daily Vaccinations Per State'))
+
+    figures.append(visualization.unwrap_figure(half_vaccine_fig.to_html()))
+
+    # half tweet figure
+    half_tweet_fig = px.scatter(x=half_x, y=half_tweet_list,
+                                title='Twitter Vaccine Perception Across US (Until Halfway Point)',
+                                labels=dict(x='Days Since 2021-02-28', y='Mean VADER Score'))
+
+    figures.append(visualization.unwrap_figure(half_tweet_fig.to_html()))
+
+    figures.append(visualization.text_block(
+        '<div style="text-align: center; font-size: 24pt; padding: 24pt">RAW DATA</div>'))
+
+    figures.append(visualization.text_block(
+        'Here is the raw data for the entire analysis (as opposed to the graphs above ' +
+        'which were half the raw data, and were only used to generate the model in ' +
+        'the section above).'))
+
+    full_x = list(range(len(vaccine_list)))
+
+    # vaccine figure
+    vaccine_fig = px.scatter(x=full_x, y=vaccine_list,
+                             title='Vaccination Rate Across US',
+                             labels=dict(x='Days Since 2021-02-28',
+                                         y='Mean Daily Vaccinations Per State'))
+
+    figures.append(visualization.unwrap_figure(vaccine_fig.to_html()))
+
+    # tweet figure
+    tweet_fig = px.scatter(x=full_x, y=tweet_list,
+                           title='Twitter Vaccine Perception Across US',
+                           labels=dict(x='Days Since 2021-02-28', y='Mean VADER Score'))
+
+    figures.append(visualization.unwrap_figure(tweet_fig.to_html()))
 
     visualization.output(figures, app.output_path, app.template_path)
