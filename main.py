baaa4a3075a123442ebce93e3350ecdc3c1e3a9b
jacob4649 <jacob.klimczak@gmail.com>
2021-12-13 20:20:08 -0500

    move location dict function out of main to avoid circular imports

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 import math
+from typing import Dict, List, Tuple
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model._glm.glm import _y_pred_deviance_derivative
@@ -8,6 +9,40 @@ import vaccinations
 import tweets
 import visualization
 from data_processing import DailyMetricCollection, average_metrics, generate_metrics, location_dict
+from tweets import Tweet
+from vaccinations import VaccinationRate
+
+
+def location_stats(vaccine_dict: Dict[str, List[VaccinationRate]],
+                   tweet_dict: Dict[str, List[Tweet]], code: str, start: date,
+                   end: date) -> Tuple[List[float], List[float]]:
+    """Return a tuple with daily mean vader scores, and daily mean vaccinations
+    for the specified state between the start and end dates
+
+    Preconditions:
+        - len(code) == 2
+        - code is a valid state code"""
+    tweets = tweet_dict[code]
+    vaccines = vaccine_dict[code]
+
+    tweet_metric = generate_metrics(
+        tweets, lambda tweet: (tweet.time_stamp.date(), tweet.polarity))
+    vaccine_metric = generate_metrics(
+        vaccines, lambda vaccine: (vaccine.time_stamp, vaccine.daily))
+
+    average_tweet_polarity = average_metrics(tweet_metric)
+    average_vaccine_rate = average_metrics(vaccine_metric)
+
+    tweet_collection = DailyMetricCollection(average_tweet_polarity, False)
+    vaccine_collection = DailyMetricCollection(average_vaccine_rate, True)
+
+    tweet_range = tweet_collection.get(start, end)
+    vaccine_range = vaccine_collection.get(start, end)
+
+    tweet_list = list(tweet_range)
+    vaccine_list = list(vaccine_range)
+
+    return tweet_list, vaccine_list
 
 
 if __name__ == '__main__':
